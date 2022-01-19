3ff9c2811d0304e92e068113c56e2b03465897cc
michalfirszt <michalfiszu@gmail.com>
2022-01-19 18:29:25 +0100

    Get current user query

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,8 +1,11 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserCreateDto } from './user.dto';
+import { UserGraphqlAuthGuard } from '../auth/guards/user-graphql-auth.guard';
+import { CurrentUserGql } from '../utilities/current-user-gql.decorator';
 
 @Resolver()
 export class UserResolver {
@@ -11,6 +14,12 @@ export class UserResolver {
   @Query(() => String)
   sayHello(): string {
     return 'Hello World!';
+  }
+
+  @UseGuards(UserGraphqlAuthGuard)
+  @Query(() => User)
+  async currentUser(@CurrentUserGql() currentUser: User): Promise<User> {
+    return currentUser;
   }
 
   @Mutation(() => User)
